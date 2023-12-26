a8b66a1a9e5e9f4952166b32207c74288e9ce9fd
haris-bit <bsef19m528@pucit.edu.pk>
2023-12-26 14:33:03 +0500

    django CRUD APIs

--- ThePicksInn/myapp/urls.py
+++ ThePicksInn/myapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.home),
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)

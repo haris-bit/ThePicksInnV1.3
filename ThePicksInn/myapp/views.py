7eb58ef43e9984af136ac41d37aaf777ab1751e6
haris-bit <bsef19m528@pucit.edu.pk>
2023-12-28 15:54:23 +0500

    html connect two APIs + data.html

--- ThePicksInn/myapp/views.py
+++ ThePicksInn/myapp/views.py
@@ -10,6 +10,42 @@ import json
 
 
 
+@api_view(['GET'])
+def home(request):
+    # actual top 20 players
+    response1 = requests.get('https://mybackendnba-e0bd8ae9accb.herokuapp.com/advance-stats/')
+    
+    # Leaderboard list data
+    response2 = requests.get('https://mybackendnba-e0bd8ae9accb.herokuapp.com/api/leaderboard-list/')
+
+    # Print status codes for debugging
+    print('Response 1 Status Code:', response1.status_code)
+    print('Response 2 Status Code:', response2.status_code)
+
+    # Check if the responses are successful (status code 200)
+    if response1.status_code == 200 and response2.status_code == 200:
+        # Get the response data as a python object. Verify that it's a dictionary.
+        data1 = response1.json()
+        data2 = response2.json()
+
+        # sort the data2 by 'accuracy', 'correct_picks_count' and 'per_percentage'
+        data2 = sorted(data2, key=lambda k: (k['accuracy'], k['correct_picks_count'], k['per_percentage']), reverse=True)
+
+        
+
+        # Pass all the data to the template
+        context = {'data1': data1, 'data2': data2}
+        return render(request, 'myapp/index.html', context)
+    else:
+        # Pass status codes to the template for debugging
+        context = {'status_code': f'{response1.status_code}, {response2.status_code}'}
+        return render(request, 'myapp/index.html', context)
+
+
+
+
+
+
 @api_view(['GET', 'POST'])
 def drink_list(request, format=None):
 
@@ -52,20 +88,3 @@ def drink_detail(request, id, format=None):
         drink.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-
-@api_view(['GET'])
-def home(request):
-    response = requests.get('https://mybackendnba-e0bd8ae9accb.herokuapp.com/advance-stats/')
-    
-    # Check if the response is successful (status code 200)
-    if response.status_code == 200:
-        data_from_api = response.json()
-    else:
-        data_from_api = None
-
-    return render(request, 'myapp/index.html', {'data_from_api': data_from_api, 'status_code': response.status_code})
-
-
-
-
-

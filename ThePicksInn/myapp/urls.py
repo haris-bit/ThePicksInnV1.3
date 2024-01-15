58b2b181139072ef5d3380acb4d1ba2c386833a1
bsef19m513 <bsef19m513@pucit.edu.pk>
2024-01-15 21:30:51 +0500

    create seperate component for leaderboard

--- ThePicksInn/myapp/urls.py
+++ ThePicksInn/myapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+
 
 urlpatterns = [
-    path('', views.top_20_players),
-    path('drinks/', views.drink_list),
-    path('drinks/<int:id>', views.drink_detail)
+    path('', views.top_20_players, name='top_20_players'),
+    path('home/', views.top_20_players, name='top_20_players'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
